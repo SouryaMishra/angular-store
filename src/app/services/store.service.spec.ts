1e89424defac151c0c0e2d55dd336f9151b2ce00
Sourya Mishra <sourya.mishra111@gmail.com>
2024-01-25 22:10:36 +0530

    add store service test

--- src/app/services/store.service.spec.ts
+++ src/app/services/store.service.spec.ts
@@ -1,12 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StoreService } from './store.service';
+import { setupSpies } from './spies';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { EnvironmentService } from './environment.service';
 
 describe('StoreService', () => {
   let service: StoreService;
+  let httpClient: any;
+  let environment: any;
+
+  const { httpClientSpy, environmentSpy } = setupSpies();
+  httpClient = httpClientSpy;
+  environment = environmentSpy;
+
+  httpClient.get.and.returnValue(
+    of([
+      {
+        id: 1,
+        title: 'Title',
+        price: 200,
+        category: 'Category',
+        description: 'Description',
+        image: 'image.png',
+      },
+    ])
+  );
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: httpClient },
+        { provide: EnvironmentService, useValue: environment },
+      ],
+    });
     service = TestBed.inject(StoreService);
   });
 
