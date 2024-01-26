bee77dc9623b42f3a8ca6d326d4191dc98e4f686
Sourya Mishra <sourya.mishra111@gmail.com>
2024-01-26 10:22:37 +0530

    add filter component tests

--- src/app/pages/home/components/filters/filters.component.spec.ts
+++ src/app/pages/home/components/filters/filters.component.spec.ts
@@ -3,27 +3,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FiltersComponent } from './filters.component';
 import { StoreService } from 'src/app/services/store.service';
 import { Observable, of } from 'rxjs';
-import { Product } from 'src/app/models/product.model';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('FiltersComponent', () => {
   let component: FiltersComponent;
   let fixture: ComponentFixture<FiltersComponent>;
-  let storeService = {
-    getAllProducts(): Observable<Product[]> {
-      return of([
-        {
-          id: 1,
-          title: 'Title',
-          price: 200,
-          category: 'Category',
-          description: 'Description',
-          image: 'image.png',
-        },
-      ]);
-    },
+  let storeService: Partial<StoreService> = {
     getAllCategories(): Observable<string[]> {
       return of(['electronics', 'shoes']);
     },
@@ -42,5 +30,42 @@ describe('FiltersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display categories', () => {
+    const panelHeader = fixture.debugElement.query(
+      By.css('mat-expansion-panel-header')
+    );
+    const listOptions = fixture.debugElement.queryAll(
+      By.css('mat-list-option')
+    );
+
+    panelHeader.triggerEventHandler('click');
+
+    expect(listOptions.length).toBe(2);
+    expect(listOptions[0].nativeElement.textContent).toContain('Electronics');
+    expect(listOptions[1].nativeElement.textContent).toContain('Shoes');
+  });
+
+  it('should emit event with selected category', () => {
+    let selectedCategory: any;
+    component.showCategory.subscribe(
+      (category) => (selectedCategory = category)
+    );
+
+    const panelHeader = fixture.debugElement.query(
+      By.css('mat-expansion-panel-header')
+    );
+    const listOptions = fixture.debugElement.queryAll(
+      By.css('mat-list-option')
+    );
+
+    panelHeader.triggerEventHandler('click');
+
+    listOptions[0].triggerEventHandler('click');
+    expect(selectedCategory).toBe('electronics');
+
+    listOptions[1].triggerEventHandler('click');
+    expect(selectedCategory).toBe('shoes');
   });
 });
